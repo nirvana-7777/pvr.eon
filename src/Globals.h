a97bd22719a519ea9cfbf122416953f9594419cb
nirvana-7777 <nirv_inc@yahoo.com>
2023-04-24 22:26:10 +0200

    Add files to git

---
+++ src/Globals.h
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (C) 2011-2021 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2011 Pulse-Eight (http://www.pulse-eight.com/)
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
+
+#include <string>
+
+static const std::string EON_USER_AGENT = std::string("Kodi/")
+    + std::string(STR(KODI_VERSION)) + std::string(" pvr.eon/")
+    + std::string(STR(EON_VERSION));
+
+static const std::string GLOBAL_URL = "global.united.cloud/";
+static const std::string BROKER_URL = "https://broker." + GLOBAL_URL;
+static const std::string PLAYER = "m3u8";
+
+static const std::string CLIENT_ID = "b8d9ade4-1093-46a7-a4f7-0e47be463c10";
+static const std::string CLIENT_SECRET = "1w4dmww87x1e9l89essqvc81pidrqsa0li1rva23";
+
+static const std::string SS_USER = "webscuser";
+static const std::string SS_SECRET = "k4md93!k334f3";
+static const std::string SS_DOMAIN = "TBA";
+static const std::string SS_PASS = "xD8iMq1!m94z";
+
+static const std::string DEVICE_TYPE = "web_linux_chrome";
+static const std::string DEVICE_NAME = "";
+static const std::string DEVICE_MODEL = "Chrome 112";
+static const std::string DEVICE_PLATFORM ="web";
+static const std::string SYSTEM_SW = "Linux";
+static const std::string SYSTEM_VERSION = "x86_64";
