e645c50d0d2a0025f0bd00a8fd37958a2b53ba20
nirvana-7777 <nirv_inc@yahoo.com>
2023-06-26 13:18:02 +0200

    Migrate from botan towards native code

---
+++ src/pkcs7_padding.hpp
@@ -0,0 +1,12 @@
+#ifndef _PKCS7_PADDING_HPP_
+#define _PKCS7_PADDING_HPP_
+
+#ifndef __cplusplus
+#error Do not include the hpp header in a c project!
+#endif //__cplusplus
+
+extern "C" {
+#include "pkcs7_padding.h"
+}
+
+#endif //_PKCS7_PADDING_HPP_
